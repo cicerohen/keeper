a7c2f07f8bcffb92794c4117011ad914053481e2
Cicero Viana <cicerohen@gmail.com>
2023-02-17 15:24:07 -0300

    chore: adds initial code.

---
+++ apps/web/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  root: true,
+  extends: ["custom"],
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error",
+  },
+};
---
+++ packages/firebase/src/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+export const test = () => {
+    console.log("blabla", process.env.TEST)
+};
+
+// export const firebase = initializeApp({
+//     apiKey: "AIzaSyB9AA99xFu01afNeMder9uEuvVB3wJEhiI",
+//     authDomain: "keeper-f7a1b.firebaseapp.com",
+//     projectId: "keeper-f7a1b",
+//     storageBucket: "keeper-f7a1b.appspot.com",
+//     messagingSenderId: "476096068554",
+//     appId: "1:476096068554:web:6a19da4fbe14185ffc2d33"
+// })
+
